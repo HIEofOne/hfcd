35533a53f592a56bf81c5662965ac7a60996351a
Zak Fallows <zakf@mit.edu>
2015-11-21 03:24:31 -0500

    Steps 1 to 3 are totally done.

--- mysite/hfcd/urls.py
+++ mysite/hfcd/urls.py
@@ -17,4 +17,10 @@ urlpatterns = [
     url(r'^help/docs/the_story/$', the_story, name='the_story'),
     url(r'^help/docs/definitions/$', definitions, name='definitions'),
     url(r'^help/docs/$', help_docs, name='help_docs'),
+    url(r'^help/step01a/$', step01a, name='step01a'),
+    url(r'^rs_hospital/step02/$', step02, name='step02'),
+    url(r'^rs_hospital/step02a/$', step02a, name='step02a'),
+    url(r'^help/step03/$', step03, name='step03'),
+    url(r'^rs_hospital/step03a/$', step03a, name='step03a'),
+    url(r'^rs_hospital/step03b/$', step03b, name='step03b'),
 ]
--- mysite/hfcd/views.py
+++ mysite/hfcd/views.py
@@ -8,7 +8,7 @@ from mysite.hfcd.models import *
 from mysite.view_helpers import multipurpose
 
 
-NUM_STEPS = 1
+NUM_STEPS = 22
 
 
 class HfcdViewsError(Exception):
@@ -95,6 +95,7 @@ class HelpPageData(PageData):
 
 def welcome(request):
     page_data = HelpPageData(you_are = None)
+    page_data.header = "HIE of One Demo"
     return render(request, 'hfcd/welcome.html', {
         'user': request.user,
         'page_data': page_data,
@@ -222,6 +223,10 @@ def definitions(request):
          "Authorization Server, this is Alice's server. This is an HIE of One server. Alice owns and controls this server."),
         ('RqP', 
          "Requesting Party, this is Dr. Bob, he requests the blood test results from the hospital."),
+        ('Principal', 
+         "This is Alice (the patient, the Resource Owner, the RO)."),
+        ('NPE', 
+         "Non-person entity. In some contexts, this refers specifically to the Big Hospital (the Resource Server, the RS)."),
     ]
     
     terms.sort(key=lambda term: term[0])
@@ -234,15 +239,18 @@ def definitions(request):
     })
 
 
-def step01(request):
-    """This is the first step of the Demo Story"""
+#=========================== The Steps of the Story ===========================#
+
+def example_step(request):
+    """Copy-paste this and then change it"""
     
-    page_data = HelpPageData(None, 1, NUM_STEPS)
-    page_title = "Step 1"
+    page_data = TodoPageData(XXyou_are, XXcurr_step, NUM_STEPS)
+    page_title = "Window Title and H1 Header"
+    next_step = 'broken_link'
     
     page_content = """
     <p>
-        This is the first step.
+        Lorem ipsum.
     </p>
     """
     
@@ -251,4 +259,191 @@ def step01(request):
         'page_title': page_title,
         'page_data': page_data,
         'page_content': page_content,
+        'next_step': next_step,
+    })
+
+
+def step01(request):
+    """This is the first step of the Demo Story"""
+    
+    page_data = HelpPageData(None, 1, NUM_STEPS)
+    page_title = "Step 1: The Beginning"
+    next_step = 'step01a'
+    
+    page_content = """
+    <p>
+        Alice is a normal person, she is not very tech-savvy. A few months ago, Alice set up an 
+        <a href="http://hieofone.org/">HIE of One</a>
+        server. It was easy, the setup process only required a credit card and an email address and/or phone number (for SMS messages). Alice's server is an Authorization Server (AS), and she can use it to grant other people access to her medical records.
+    </p>
+    
+    <p>
+        Today, Alice is going to the hospital for blood tests. She wants to share her blood test results with her doctor, Bob, but he is not affiliated with the hospital.
+    </p>
+    
+    <p>
+        Starting now, you will be impersonating Alice.
+    </p>
+    """
+    
+    return render(request, 'hfcd/normal_step.html', {
+        'user': request.user,
+        'page_title': page_title,
+        'page_data': page_data,
+        'page_content': page_content,
+        'next_step': next_step,
+    })
+
+
+def step01a(request):
+    page_data = HelpPageData(alice_str, 1, NUM_STEPS)
+    page_title = "Alice at the Hospital"
+    next_step = 'step02'
+    
+    page_content = """
+    <p>
+        You (Alice) go to the hospital and get your blood drawn. The hospital analyzes your blood and stores the results in an electronic health record (EHR) on a server owned by the hospital.
+    </p>
+    
+    <p>
+        The hospital has a website, a patient gateway. This website allows you to access your own health records. In order to access your health records, you will need a password.
+    </p>
+    
+    <p>
+        In order to get a password for the hospital patient gateway, you go to the receptionist and show your driver's license to identify yourself. Now that the hospital has verified your identity, they will allow you to create a password.
+    </p>
+    
+    <p>
+        The receptionist hands you an iPad.
+    </p>
+    """
+    
+    return render(request, 'hfcd/normal_step.html', {
+        'user': request.user,
+        'page_title': page_title,
+        'page_data': page_data,
+        'page_content': page_content,
+        'next_step': next_step,
+    })
+
+
+def step02(request):    
+    page_data = HospitalPageData(alice_str, 2, NUM_STEPS)
+    page_title = "Choose a Password for the Patient Gateway"
+    next_step = 'step02a'
+    
+    page_content = """
+    <p>
+        Hello Alice. Please choose a password and enter it below. After you go home, you can use this password to login and view your hospital records.
+    </p>
+    
+    <p>
+        New Password:
+        <input type="password" size="20" />
+        (Demo note: Your input will be ignored.)
+        
+        <br /><br />
+        
+        Repeat Password:
+        <input type="password" size="20" />
+    </p>
+    """
+    
+    return render(request, 'hfcd/normal_step.html', {
+        'user': request.user,
+        'page_title': page_title,
+        'page_data': page_data,
+        'page_content': page_content,
+        'next_step': next_step,
+    })
+
+
+def step02a(request):    
+    page_data = HospitalPageData(alice_str, 2, NUM_STEPS)
+    page_title = "Thank You for Choosing a Password"
+    next_step = 'step03'
+    
+    page_content = """
+    <p>
+        Your password has been saved.
+    </p>
+    """
+    
+    return render(request, 'hfcd/normal_step.html', {
+        'user': request.user,
+        'page_title': page_title,
+        'page_data': page_data,
+        'page_content': page_content,
+        'next_step': next_step,
+    })
+
+
+def step03(request):
+    page_data = HelpPageData(alice_str, 3, NUM_STEPS)
+    page_title = "Alice Goes Home"
+    next_step = 'step03a'
+    
+    page_content = """
+    <p>
+        Having created a password, you drive home. Once at home, you open your laptop and you go to BigHospital.com to access your medical records.
+    </p>
+    """
+    
+    return render(request, 'hfcd/normal_step.html', {
+        'user': request.user,
+        'page_title': page_title,
+        'page_data': page_data,
+        'page_content': page_content,
+        'next_step': next_step,
+    })
+
+
+def step03a(request):    
+    page_data = HospitalPageData(alice_str, 3, NUM_STEPS)
+    page_title = "Enter Your Patient Gateway Password"
+    next_step = 'step03b'
+    
+    page_content = """
+    <p>
+        Welcome to the Big Hospital website. You can use this website to access or share your medical records.
+    </p>
+    
+    <p>
+        Username:
+        <input type="text" value="Alice" size="30" />
+        
+        <br /><br />
+        
+        Password:
+        <input type="password" size="20" />
+        (Demo note: You can type anything.)
+    </p>
+    """
+    
+    return render(request, 'hfcd/normal_step.html', {
+        'user': request.user,
+        'page_title': page_title,
+        'page_data': page_data,
+        'page_content': page_content,
+        'next_step': next_step,
+    })
+
+
+def step03b(request):    
+    page_data = HospitalPageData(alice_str, 3, NUM_STEPS)
+    page_title = "Thank You for Logging In"
+    next_step = 'broken_link'
+    
+    page_content = """
+    <p>
+        Thank you for logging in.
+    </p>
+    """
+    
+    return render(request, 'hfcd/normal_step.html', {
+        'user': request.user,
+        'page_title': page_title,
+        'page_data': page_data,
+        'page_content': page_content,
+        'next_step': next_step,
     })
